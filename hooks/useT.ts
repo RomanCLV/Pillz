8d62c981797aabe8ae33f087a9a7dde1b0b45754
RomanCLV <roman.clavier.2001@gmail.com>
2025-12-16 18:37:34 +0100

    rm empty file

--- hooks/useT.ts
+++
@@ -1,8 +0,0 @@
-import { translate } from "@i18n/t";
-import { useCurrentLanguage } from "./useCurrentLanguage";
-
-export const useT = () => {
-  const lang = useCurrentLanguage();
-  return (path: string, vars: Record<string, string | number> = {}, pluralize: boolean = false) =>
-    translate(lang, path, vars, pluralize);
-};
